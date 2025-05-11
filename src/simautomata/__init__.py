25246f57bb1f491155ce0b514df3284c974b61f7
AdrianC <112999083+AdrianVMC@users.noreply.github.com>
2025-05-11 17:54:29 -0600

    Delete src/simautomata directory

--- src/simautomata/__init__.py
+++
@@ -1,6 +0,0 @@
-# simautomata/src/__init__.py
-
-def saludar(nombre: str) -> str:
-    if not nombre.strip():
-        return "⚠️ Por favor, escribe tu nombre."
-    return f"👋 Hola, {nombre}. Bienvenido al simulador de autómatas."
